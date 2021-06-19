2d7898d17db301d94af54a22b8d4d48824926b99
Lara Schneeberger Montero <laraschm27@gmail.com>
2021-06-18 22:03:23 -0300

    Actualizacion

---
+++ JS/cartelera.js
@@ -0,0 +1,31 @@
+let contenedor = document.querySelector('.contenedor-ofertas');
+for (const oferta of listadoOfertas) {
+    contenedor.innerHTML += `<div class="busqueda">
+    <span>${oferta.zona}<br>${oferta.categoria}<br>${oferta.observaciones}<br></span>
+    <img src="${oferta.imgUrl}" alt="img-mapa" class="img-mapa">
+    <button type="submit" class="btn">Postular</button>
+</div>`
+}
+
+let botones = document.querySelectorAll('.btn');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', function () {
+        let divs = document.querySelector('.postular-form');
+        divs.classList.toggle('postular-form')
+        divs.classList.toggle('postular-form-dos');
+    })
+})
+
+let botonCross = document.querySelector('#cross');
+botonCross.addEventListener('click', function () {
+    let divs = document.querySelector('.postular-form-dos');
+    divs.classList.toggle('postular-form');
+    divs.classList.toggle('postular-form-dos');
+})
+let botonEnviar = document.querySelector('.enviar');
+botonEnviar.addEventListener('click', function () {
+    let divs = document.querySelector('.postular-form-dos');
+    divs.classList.toggle('postular-form');
+    divs.classList.toggle('postular-form-dos');
+})
---
+++ JS/listado-ofertas.js
@@ -0,0 +1,31 @@
+const listadoOfertas = [
+    {
+        zona: 'Zona: centrica',
+        categoria: 'Entra en una caja',
+        observaciones: 'Lorem imput',
+        imgUrl: '../IMG/gmap.png' 
+        
+    },
+    {
+        zona: 'Zona: centrica',
+        categoria: 'Entra en una caja',
+        observaciones: 'Lorem imput',
+        imgUrl: '../IMG/gmap.png' 
+        
+    },
+    {
+        zona: 'Zona: centrica',
+        categoria: 'Entra en una caja',
+        observaciones: 'Lorem imput',
+        imgUrl: '../IMG/gmap.png' 
+        
+    },
+    {
+        zona: 'Zona: centrica',
+        categoria: 'Entra en una caja',
+        observaciones: 'Lorem imput',
+        imgUrl: '../IMG/gmap.png' 
+        
+    }
+
+]
